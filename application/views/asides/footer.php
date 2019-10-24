91edb3b0c37c1b2d15cbcaa3d5cb3f2a4c2cb1c5
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-10-24 12:52:31 +0700

    new product

--- application/views/asides/footer.php
+++ application/views/asides/footer.php
@@ -8,7 +8,7 @@
   <script src="<?= assets_url() ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?= assets_url() ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
   <script src="<?= assets_url() ?>js/sb-admin-2.min.js"></script>
--- application/views/asides/header.php
+++ application/views/asides/header.php
@@ -18,7 +18,7 @@
   crossorigin="anonymous"></script>
   <link href="<?= assets_url() ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+  <link href="<?= assets_url() ?>css/style.css" rel="stylesheet" type="text/css">
 
  <!-- Toastr -->
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
@@ -26,7 +26,7 @@
 
 
   <!-- Custom styles for this template-->
-  <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="<?= assets_url() ?>css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 <body id="page-top">
