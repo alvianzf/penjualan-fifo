80903cb76434715ae8130a7cfce08f13583f3331
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-10-24 12:06:46 +0700

    forms

--- application/views/asides/sidebar.php
+++ application/views/asides/sidebar.php
@@ -29,7 +29,7 @@
       </div>
 
       <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
+      <li class="nav-item <?= nav(['new', 'inventory'], $this->uri->segments) ?>">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
           <i class="fas fa-fw fa-industry"></i>
           <span>Produksi</span>
@@ -37,25 +37,8 @@
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Produksi Bata</h6>
-            <a class="collapse-item" href="buttons.html">Produksi</a>
-            <a class="collapse-item" href="cards.html">Inventaris</a>
-          </div>
-        </div>
-      </li>
-
-      <!-- Nav Item - Utilities Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-          <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
+            <a class="collapse-item" href="">Produksi</a>
+            <a class="collapse-item" href="">Inventaris</a>
           </div>
         </div>
       </li>
