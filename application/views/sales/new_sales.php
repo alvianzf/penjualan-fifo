f9729a3802de1c455737d5ba41813887457ea402
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-10-24 16:25:29 +0700

    add sales

---
+++ application/views/sales/new_sales.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="card shadow border-left-primary col-md-12">
+        <div class="card-body">
+            <h4>Penjualan Baru</h4>
+        </div>
+    </div>
+</div>
