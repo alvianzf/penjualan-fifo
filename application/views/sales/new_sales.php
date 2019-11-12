7bcb61f28c5c0b76b1eba1951eb30ac349e206de
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-11-12 13:46:48 +0700

    purchase

--- application/views/sales/new_sales.php
+++ application/views/sales/new_sales.php
@@ -3,5 +3,27 @@
         <div class="card-body">
             <h4>Penjualan Baru</h4>
         </div>
+    
+        <div class="card-body">
+
+            <div class="row">
+                <div class="col-xs-12 col-md-4">
+                    <div class="form-group">
+                        <label for="pembeli">Pembeli<sup class="text-danger">*</sup></label>
+                        <input id="pembeli" class="form-control" placeholder="nama pembeli">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                    <div class="form-group">
+                        <label for="item">Item<sup class="text-danger">*</sup></label>
+                        <select id="item" class="form-control">
+                            <option value="" disabled selected>Pilih</option>
+                            <option>Bata</option>
+                            <option>Lainnya</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
