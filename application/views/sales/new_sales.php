2d1981c73ffa336d6e618915135e1512833a60a0
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-11-12 14:15:41 +0700

    user and transactions

--- application/views/sales/new_sales.php
+++ application/views/sales/new_sales.php
@@ -23,7 +23,72 @@
                         </select>
                     </div>
                 </div>
+                <div class="col-xs-12 col-md-1">
+                    <label for="stock">Sisa stok</label>
+                    <p id="stock"></p>
+                </div>
+                <div class="col-xs-12 col-md-1">
+                    <label for="harga">Harga</label>
+                    <p id="harga"></p>
+                </div>
+                <div class="col-xs-12 col-md-2">
+                    <div class="form-group">
+                        <label for="jumlah">Jumlah<sup class="text-danger">*</sup></label>
+                        <input type="number" id="jumlah" class="form-control" placeholder="jumlah pembelian">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-1">
+                    <label for="stock">Subtotal</label>
+                    <p id="subtotal"></p>
+                </div>
             </div>
         </div>
     </div>
+</div>
+
+    <br />
+
+<div class="row">
+    <div class="card shadow border-left-primary col-md-12">
+
+        <div class="card-body">
+            <h4>Daftar pembelian</h4>
+
+            <div class="col-xs-12 col-md-12">
+                <table id="table" class="table table-striped table-condensed table-hover table-collapse" width="100%">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Item</th>
+                            <th>Jumlah</th>
+                            <th>Harga</th>
+                            <th>Subtotal</th>
+                            <th><i class="fa fa-cog"></i></th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <hr>
+
+            <div class="row">
+                <div class="col-xs-12 col-md-3">
+                    <select id="tipe" class="form-control" >
+                        <option value="" selected disabled>Tipe Pembayaran</option>
+                        <option>Tunai</option>
+                        <option>Bertahap</option>
+                    </select>
+                </div> 
+                <div class="col-xs-12 col-md-3">
+                    <button id="ok" class="btn btn-success btn-block"><i class="fa fa-check"></i> OK</button>
+                </div> 
+                <div class="col-xs-12 col-md-3">
+                    <button id="clear" class="btn btn-warning btn-block"><i class="fa fa-list"></i> Clear</button>
+                </div> 
+                <div class="col-xs-12 col-md-3">
+                    <button id="list" class="btn btn-info btn-block"><i class="fa fa-money"></i> Daftar Transaksi</button>
+                </div> 
+            </div>
+        </div>
+    </div>
+
 </div>
---
+++ application/views/user/index.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="car shadow border-left-primary col-md-12">
+        <div class="card-body">
+            OK
+        </div>
+    </div>
+</div>
