82e67d14ddf7030fd37abddbce98b7070f7204aa
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-12-02 23:53:51 +0700

    helper change and setup

--- application/helpers/My_helper.php
+++ application/helpers/My_helper.php
@@ -1,30 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-/**
- *
- * Helpers My_helper_helper
- *
- * This Helpers for ...
- * 
- * @package   CodeIgniter
- * @category  Helpers
- * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
- * @link      https://github.com/setdjod/myci-extension/
- *
- */
-
-// ------------------------------------------------------------------------
-
 if (!function_exists('check')) {
-  /**
-   * Check
-   *
-   * To check output to view
-   *
-   * @param   ...
-   * @return  ...
-   */
   function check($data)
   {
     // 
@@ -36,51 +13,24 @@ if (!function_exists('check')) {
 
 
 if (!function_exists('api')) {
-  /**
-   * Test
-   *
-   * This test helpers
-   *
-   * @param   ...
-   * @return  ...
-   */
   function api($string = null)
   {
-    // 
     return base_url() . 'v1/' . $string . '/';
   }
 }
 
 
 if (!function_exists('assets_url')) {
-  /**
-   * Test
-   *
-   * This test helpers
-   *
-   * @param   ...
-   * @return  ...
-   */
   function assets_url($string = null)
   {
-    // 
     return base_url() . 'assets/' . $string;
   }
 }
 
 
 if (!function_exists('nav')) {
-  /**
-   * Test
-   *
-   * This test helpers
-   *
-   * @param   ...
-   * @return  ...
-   */
   function nav($nav = null, $array)
   {
-    // 
     return in_array($nav, $array) ? 'active' : null;
   }
 
@@ -98,7 +48,3 @@ if (!function_exists('nav')) {
     }
   }
 }
-// ------------------------------------------------------------------------
-
-/* End of file My_helper_helper.php */
-/* Location: ./application/helpers/My_helper_helper.php */
