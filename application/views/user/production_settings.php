f53153fbc6bcfce8e72c1883c4767ec6f94a2671
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-12-16 10:17:05 +0700

    input group text

---
+++ application/views/user/production_settings.php
@@ -0,0 +1,28 @@
+<div class="row">
+    <div class="card shadow border-left-primary col-md-12">
+        <div class=" card-body col-xs-12 col-md-12">
+            <h4>Atur detil produksi</h4>
+        </div>
+
+        <div class="card-body">
+            <div class="row">
+                <div class="form-group col-md-6 col-xs-12">
+                    <label for="type">Tipe Barang</label>
+                    <input id="type" class="form-control" placeholder="Tipe Barang" />
+                </div>
+                <div class="form-group col-md-6 col-xs-12">
+                    <label for="harga">Harga Barang</label>
+                    <div class="input-group">
+                        <span class="input-group-text">Rp.</span>
+                        <input id="harga" class="form-control" type="number" placeholder="Harga Barang" />
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group col-md-4 col-xs-12 text-right">
+                        <button id="add-btn" class="btn btn-success btn-block" ><span class="fa fa-cog"></span> Tambah Produksi</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
