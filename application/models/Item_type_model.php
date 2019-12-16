748739e4890da253482e256f622a64c8f66aae1b
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-12-16 14:47:18 +0700

    item type

---
+++ application/models/Item_type_model.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed!');
+
+class Item_type_model extends MY_Model
+{
+    protected $_table = 'item_type';
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
