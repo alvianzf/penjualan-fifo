710e95ca9aff0d171a18afd68ba2c1e29dd95972
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-12-18 17:07:44 +0700

    payment post

--- application/controllers/v1/Payment.php
+++ application/controllers/v1/Payment.php
@@ -9,3 +9,39 @@ class Payment extends REST_Controller
         parent::__construct();
         $this->load->model(['transactions_model', 'payment_model']);
     }
+
+    public function data_get($id)
+    {
+        $data = $this->payment_model->get_by('transaction_id', $id);
+
+        if ($data)
+            return $this->response(api_success($data), 200);
+
+        return $this->response(api_error($data), 500);
+    }
+
+    public function pay_post()
+    {
+        $id     = $this->post('transaction_id');
+        $nominal= $this->post('jumlah');
+        $qty    = $this->post('qty');
+        $nominal= $this->post('nominal');
+        $ket    = $this->post('keterangan');
+
+        $data = [
+            'transaction_id'    => $id,
+            'tanggal'           => time(),
+            'qty'               => $qty,
+            'nominal'           => $nominal,
+            'keterangan'        => $ket,
+            'created_at'        => time()
+        ];
+
+        if ($this->payment_model->insert($data))
+            return $this->response(api_success($data), 200);
+
+        return $this->response(api_error($data), 500);
+
+    }
+
+}
