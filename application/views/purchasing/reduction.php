9b4b6376f5e142bde7e882006f7a9243bcf35f94
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2020-01-13 18:38:43 +0700

    stock

---
+++ application/views/purchasing/reduction.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="card shadow border-left-primary col-md-12">
+        <div class="card-body">
+            <h4>Pengambilan Stok</h4>
+        </div>
+    </div>
+</div
